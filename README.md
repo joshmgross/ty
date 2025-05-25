aa519b49154536c40cef6871cf5001cd0f718ca3
Josh Gross <joshuamgr@gmail.com>
2025-05-25 19:42:01 -0400

    Add basic command handler structure

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ A Discord Bot to show some thanks
 
 ## Requirements
 
+1. A Cloudflare account for the Workers
 1. A Discord app with the permissions to send messages and use slash commands
     * See [configuration instructions from the Discord sample](https://discord.com/developers/applications)
-1. A Cloudflare account for the Workers
+    * The "Interactions Endpoint URL" under "General Information" should be set to your CF Workers URL
 
 ## Configuration
 
@@ -16,6 +17,14 @@ Create a `.dev.vars` file with the following values
 DISCORD_APPLICATION_ID:
 DISCORD_PUBLIC_KEY:
 DISCORD_TOKEN:
+```
+
+These should also be set in your Cloudflare project:
+
+```shell
+npx wrangler secret put DISCORD_APPLICATION_ID
+npx wrangler secret put DISCORD_PUBLIC_KEY
+npx wrangler secret put DISCORD_TOKEN
 ```
 
 Follow the instructions in https://developers.cloudflare.com/workers/ci-cd/external-cicd/github-actions/
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,58 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { InteractionResponseType, InteractionType, verifyKey } from "discord-interactions";
+import { AutoRouter, IRequest } from "itty-router";
+import { TY_COMMAND } from "./commands";
+
+const router = AutoRouter()
+
+router.get('/', (_request, _env) => {
+	return new Response(`👋`);
+});
+
+router.post('/', async (request, env) => {
+	const interaction = await getInteraction(request, env);
+	if (!interaction) {
+		return new Response('Unauthorized', { status: 401 });
+	}
+
+	switch (interaction.type) {
+		case InteractionType.PING:
+			return new Response(
+				JSON.stringify({ type: InteractionResponseType.PONG }),
+				{ headers: { 'Content-Type': 'application/json' } }
+			);
+		case InteractionType.APPLICATION_COMMAND:
+			switch (interaction.data.name.toLowerCase()) {
+				case TY_COMMAND.name:
+					return new Response(
+						JSON.stringify({ type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE, data: { content: "You're welcome!" } }),
+						{ headers: { 'Content-Type': 'application/json' } }
+					);
+				default:
+					return new Response('Unknown command.', { status: 400 });
+			}
+
+		default:
+			return new Response('Unknown interaction type.', { status: 400 });
+	}
+});
+
+router.all('*', () => new Response('Not Found.', { status: 404 }));
+
+async function getInteraction(request: IRequest, env: Env): Promise<any | undefined> {
+	const signature = request.headers.get('X-Signature-Ed25519');
+	const timestamp = request.headers.get('X-Signature-Timestamp');
+	if (!signature || !timestamp) {
+		return;
+	}
+
+	const body = await request.text();
+	if (!await verifyKey(body, signature, timestamp, env.DISCORD_PUBLIC_KEY)) {
+		return;
+	}
+
+	return JSON.parse(body);
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
+	fetch: router.fetch,
 } satisfies ExportedHandler<Env>;
